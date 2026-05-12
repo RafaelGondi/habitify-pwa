07f80344cfd6b8460a6939cf74cccd8866315e1d
“RafaelGondi” <rdias.ga@gmail.com>
2026-05-12 15:22:18 -0300

    Hardcode Firebase config in plugin, remove runtimeConfig

--- app/plugins/firebase.client.ts
+++ app/plugins/firebase.client.ts
@@ -2,11 +2,17 @@ import { initializeApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyA5INhuvkDhpaRC9-iPw6Ukc1XLqVu0LWA',
+  authDomain: 'habitify-pwa-3399c.firebaseapp.com',
+  projectId: 'habitify-pwa-3399c',
+  storageBucket: 'habitify-pwa-3399c.firebasestorage.app',
+  messagingSenderId: '993186156958',
+  appId: '1:993186156958:web:a7015bddd46f705f21468f',
+}
+
 export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig()
-
-  const app = getApps()[0] ?? initializeApp(config.public.firebase as object)
-
+  const app = getApps()[0] ?? initializeApp(firebaseConfig)
   const db = getFirestore(app)
   const auth = getAuth(app)
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,19 +8,6 @@ export default defineNuxtConfig({
 
   ssr: false,
 
-  runtimeConfig: {
-    public: {
-      firebase: {
-        apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
-      },
-    },
-  },
-
   devtools: {
     enabled: true,
   },
